2fb65ac5f0dfa68c54147f70f5c9e65e22572694
Arthur <arthurdesouzasantana71@gmail.com>
2026-09-22 11:46:41 -0300

    refactor: remove unused imports and clean up App component

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
 
@@ -9,8 +7,6 @@ export default function App() {
     
     <GluestackUIProvider mode="dark">
       <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
     </GluestackUIProvider>
   

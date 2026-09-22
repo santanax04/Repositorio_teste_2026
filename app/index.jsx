471191beebf5ce627bacba6efc14f1b7f539b1bc
Arthur <arthurdesouzasantana71@gmail.com>
2026-09-22 19:36:30 -0300

    feat: add Divider component for improved layout separation in App

--- app/index.jsx
+++ app/index.jsx
@@ -10,17 +10,22 @@ export default function App() {
     <GluestackUIProvider mode="dark">
       <View>
         <Text>Página Home</Text>
+        <Divider className="my-3 w-52"/>
+
         <Button className='bg-fuchsia-300'>
           <ButtonText className='font-bold'>Primeiro Página</ButtonText>
         </Button>
+        <Divider className="my-3 w-20"/>
 
         <Button className='bg-fuchsia-300'>
           <ButtonText className='font-bold'>Segundo Página</ButtonText>
         </Button>
+        <Divider className="my-3 w-20"/>
 
         <Button className='bg-fuchsia-300'>
           <ButtonText className='font-bold'>Terceira Página</ButtonText>
         </Button>
+        <Divider className="my-3 w-20"/>
       </View>
     </GluestackUIProvider>
   

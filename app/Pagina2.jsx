884427e85f9f00d3e802d6276e2b2e108ff50e13
Arthur <arthurdesouzasantana71@gmail.com>
2026-09-22 19:50:32 -0300

    feat: add multiple page components with consistent styling and layout

---
+++ app/Pagina2.jsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+import { Button, ButtonText } from '@/components/ui/button';
+
+
+export default function App() {
+  return (
+    
+    <GluestackUIProvider mode="dark">
+      <View>
+        <Text className="uppercase font-extrabold">Página Home</Text>
+        <Divider className="my-3 w-52"/>
+
+        <Button className='bg-fuchsia-300'>
+          <ButtonText className='font-bold text-white'>Primeiro Página</ButtonText>
+        </Button>
+        <Divider className="my-3 w-20"/>
+
+        <Button className='bg-indigo-600'>
+          <ButtonText className='font-bold text-white'>Segundo Página</ButtonText>
+        </Button>
+        <Divider className="my-3 w-20"/>
+
+        <Button className='bg-slate-800'>
+          <ButtonText className='font-bold text-white'>Terceira Página</ButtonText>
+        </Button>
+        <Divider className="my-3 w-52"/>
+      </View>
+    </GluestackUIProvider>
+  
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

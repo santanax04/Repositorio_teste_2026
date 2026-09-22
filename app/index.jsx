c34e86b88de4ddf91347fa716f4e94555fa8e709
Arthur <arthurdesouzasantana71@gmail.com>
2026-09-22 12:38:57 -0300

    feat: create initial App component with GluestackUIProvider and styled Button

---
+++ app/index.jsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+import { Button, ButtonText } from './components/ui/button';
+
+
+export default function App() {
+  return (
+    
+    <GluestackUIProvider mode="dark">
+      <View style={styles.container}>
+        <Text>Página com Expo Router</Text>
+        <Button className='bg-fuchsia-300'>
+          <ButtonText>Primeiro Botão</ButtonText>
+        </Button>
+      </View>
+    </GluestackUIProvider>
+  
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

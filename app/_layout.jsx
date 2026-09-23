e0062056e300195f7fa82fa2be674417f4a32a06
Arthur <arthurdesouzasantana71@gmail.com>
2026-09-23 10:33:58 -0300

    refactor: remove SafeAreaView for cleaner layout in _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,13 +3,11 @@ import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
 import { Tabs } from "expo-router";
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
 
     <GluestackUIProvider mode="dark">
-      <SafeAreaView className="flex-1 bg-white">
         <Tabs initialRouteName="Pagina1" screenOptions={{ headerShown: false }}>
           <Tabs.Screen name="Pagina1" options={{ title: "Home", tabBarIcon: ({color, size})=>(
             <Ionicons name="home-outline" color={color} size={size} />
@@ -27,7 +25,6 @@ export default function App() {
 
           <Tabs.Screen name="index" options={{ href: null }} />
         </Tabs>
-      </SafeAreaView>
     </GluestackUIProvider>
 
   );
